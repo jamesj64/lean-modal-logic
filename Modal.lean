b523336c90e86e86c52ba5dccb4a7809feb01b34
James <102470405+jamesj64@users.noreply.github.com>
2025-09-23 08:56:36 -0400

    Enhance Modal.lean with Propositional imports

--- Modal.lean
+++ Modal.lean
@@ -1,3 +1,8 @@
 -- This module serves as the root of the `Modal` library.
 -- Import modules here that should be built as part of the library.
 import «Modal».Basic
+-- Modal.lean
+import Modal.Propositional.Syntax
+import Modal.Propositional.Semantics
+import Modal.Propositional.Soundness
+-- etc, one line per file in Modal/Propositional/
